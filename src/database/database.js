53993be65bbf23761d81a6f0b9446c693ca298e7
gavuolo <gabs.vuolo@gmail.com>
2023-02-27 17:44:36 -0400

    Fix: database atualizado para avaliador

--- src/database/database.js
+++ src/database/database.js
@@ -5,10 +5,19 @@ dotenv.config();
 
 const { Pool } = pkg;
 
-const configDatabase = {
-  connectionString: process.env.DATABASE_URL,
-};
+
 if(process.env.MODE === 'prod'){
   configDatabase.ssl = true;
 }
+
+
+
+const configDatabase = {
+  connectionString: process.env.DATABASE_URL,
+  ...(process.env.NODE_ENV === "production" && {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  }),
+};
 export const db = new Pool(configDatabase)
