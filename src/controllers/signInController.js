65e9c86e26b7dc4fb7a493126d7fb9231b65d3f6
gavuolo <gabs.vuolo@gmail.com>
2023-03-01 13:35:46 -0400

    Fix: error non-existent email

--- src/controllers/signInController.js
+++ src/controllers/signInController.js
@@ -2,31 +2,33 @@ import { db } from "../database/database.js";
 import { v4 as uuidV4 } from "uuid";
 import bcrypt from "bcrypt";
 
-export async function postSignIn(req, res){
-    const {email, password} = res.locals.signIn
-    try{
-        const user = await db.query(
-            `
+export async function postSignIn(req, res) {
+  const { email, password } = res.locals.signIn;
+  try {
+    const user = await db.query(
+      `
             SELECT * FROM users WHERE email=$1
-            `, [email]
-        )
-        if(user.rowCount > 0){
-            return res.sendStatus(401)
-        }
-        const comparePassword = bcrypt.compareSync(password, user.rows[0].password)
-        if(!comparePassword){
-            return res.sendStatus(422)
-        }
-        const token = uuidV4()
-        const userId = user.rows[0].id
-        await db.query(
-            `
+            `,
+      [email]
+    );
+    if (user.rows[0] === undefined) {
+      return res.sendStatus(401);
+    }
+    const comparePassword = bcrypt.compareSync(password, user.rows[0].password);
+    if (!comparePassword) {
+      return res.sendStatus(422);
+    }
+    const token = uuidV4();
+    const userId = user.rows[0].id;
+    await db.query(
+      `
             INSERT INTO sessions (token, "userId")
             VALUES ($1, $2)
-            `, [token, userId]
-        )
-        res.send({token}).status(201)
-    }catch(error){
-        res.send(error.message).status(400)
-    }
+            `,
+      [token, userId]
+    );
+    res.send({ token }).status(201);
+  } catch (error) {
+    res.send(error.message).status(400);
+  }
 }
